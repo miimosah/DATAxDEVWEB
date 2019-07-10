c35e210884000b3a6ec9903b03abd39e61f3f10f
Keltouma B <k.boujaoud@gmail.com>
2019-07-10 16:08:14 +0200

    Connexion de la BDD à l'API

--- API/api.js
+++ API/api.js
@@ -3,6 +3,25 @@ const express = require('express');
 const app = express();
 var bodyParser = require('body-parser')
 
+
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "78929313k",
+    database:"chatnanterre",
+});
+
+con.connect(function (err) {
+
+    if (!err){
+        console.log("Connected!");
+
+    }else
+    console.log(err); 
+    
+    
+});
+
 app.listen(8000);
 console.log('votre API est enligne');
 app.use(bodyParser.urlencoded({ extended: true }));
